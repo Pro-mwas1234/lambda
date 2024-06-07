a125edad8ad5e7e8c8e8b83f43147393c8b952c3
shawn-john7 <shawnmwask1234@outlook.com>
2024-06-07 12:24:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
+
+
+  function login(){
+    alert("Y ou have sucessfully logged in")
+  }
+ function logout(){
+   window
+ }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
